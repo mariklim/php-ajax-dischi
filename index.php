aca43f262ce83d6de3d99a3d935666cdc092f414
mariklim <maria.klimenko889@gmail.com>
2021-10-21 15:34:58 +0200

    add tes get image from database

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.scss">
-    <title>Document</title>
+    <title>Spotify PHP</title>
 </head>
 <body>
 
@@ -19,9 +19,27 @@ include __DIR__ . "/partials/data.php";
 
 <!--HTML -->
 <?php include __DIR__ . "/partials/template/header.php"; ?>
-<main></main>
+<main>
+    <div class="container_centered">
+        <div class="cards">
+            <div class="card">
+                <div class="card-img">
+                <img src=<?php echo $database[0]['poster']?>
+                 alt="<?php echo $database[0]['title']?>">   
+                <?php echo $database[0]['title']?>
+                
+            
+                </div>
+                <div class="card-description">Ciao</div>
+            </div>
+        </div>
+    </div>
+</main>
 
+
+<!-- JS, VUE.JS -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-<script src="js/script.js"></script>    
+<script src="js/script.js"></script>
+<!-- JS, VUE.JS -->  
 </body>
 </html>
