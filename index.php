53e6557f67780b8bff6a039dacd20b147d758116
mariklim <maria.klimenko889@gmail.com>
2021-10-21 15:49:20 +0200

    add cycle PHP for albums

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,39 +8,51 @@
     <link rel="stylesheet" href="style/style.scss">
     <title>Spotify PHP</title>
 </head>
+
 <body>
 
-<!-- PHP -->
-<?php
-include __DIR__ . "/partials/data.php";
-// var_dump($database);
-// phpinfo();
-?>
-<!-- /PHP -->
+    <!-- PHP -->
+    <?php
+    include __DIR__ . "/partials/data.php";
+    // var_dump($database);
+    // phpinfo();
+    ?>
+    <!-- /PHP -->
 
-<!--HTML -->
-<?php include __DIR__ . "/partials/template/header.php"; ?>
-<main>
-    <div class="container_centered">
-        <div class="cards">
-            <div class="card">
-                <div class="card-img">
-                <img src=<?php echo $database[0]['poster']?>
-                 alt="<?php echo $database[0]['title']?>">   
-                <?php echo $database[0]['title']?>
-                
-            
-                </div>
-                <div class="card-description">Ciao</div>
+    <!--HTML -->
+    <?php include __DIR__ . "/partials/template/header.php"; ?>
+    <main>
+        <div class="container_centered">
+            <div class="cards">
+
+            <!-- cycle PHP -->
+                <?php foreach ($database as $data) : ?>
+
+                    <!-- Album view HTML  -->
+                    <div class="card">
+                        <div class="card-img">
+                            <img src=<?php echo $data['poster'] ?> alt="<?php echo $data['title'] ?>">
+                        </div>
+                        <div class="card-description">
+                            <h3><?php echo $database[0]['title'] ?></h3>
+                            <p><?php echo $database[0]['author'] ?></p>
+                            <p><?php echo $database[0]['year'] ?></p>
+                        </div>
+                    </div>
+                   <!--/ Album view HTML  -->
+
+                <?php endforeach; ?>
+                <!-- /cycle PHP -->
+
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
 
-<!-- JS, VUE.JS -->
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-<script src="js/script.js"></script>
-<!-- JS, VUE.JS -->  
+    <!-- JS, VUE.JS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="js/script.js"></script>
+    <!-- JS, VUE.JS -->
 </body>
+
 </html>
