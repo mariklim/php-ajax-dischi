9e508ed735ad8290042b77880dc0dbea183ded0c
mariklim <maria.klimenko889@gmail.com>
2021-10-22 16:50:28 +0200

    add filter

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 require __DIR__ . "/../partials/data.php";
 header("Content-Type:application/json");
-echo json_encode($database);
+
+if(!empty($_GET['genre'])){
+    $genre = $_GET['genre'];
+    $dbFiltered = [];
+
+    foreach($database as $album){
+        if($genre == $album['genre']){
+            $dbFiltered[] = $album; 
+        }
+    }
+    echo json_encode($dbFiltered);
+}else{
+    echo json_encode($database);
+}
--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 const app = new Vue({
-    el: "#app",
-    data:{
-        albums: null,
+  el: "#app",
+  data: {
+    albums: [],
+    genres: [],
+    select: "",
 
-    },
-    created(){
-        axios.get('http://localhost/api/index.php')
-  .then((response) => {
-    this.albums = response.data;
-    // console.log(this.albums); 
-    
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+  },
+  created() {
+    axios.get('http://localhost/api/index.php')
+      .then((response) => {
+        this.albums = response.data;
+      })
+
+      axios.get('http://localhost/api/index.php?genres=genres')
+      .then((response) => {
+        this.genres = response.data;
+      })
+
+  },
+  methods: {
+    onChange(event) {
+      this.select = event.target.value;
+      axios.get('http://localhost/api/index.php', {
+        params: {
+          genre: this.select,
+        },
+      })
+        .then((response) => {
+          this.albums = response.data;
+        });
+
+
     }
+  },
 })
