902280a8afc4d1dcbd5ba18fefc6e2588a066a1a
mariklim <maria.klimenko889@gmail.com>
2021-10-22 17:56:17 +0200

    add comments

--- api/index.php
+++ api/index.php
@@ -2,16 +2,27 @@
 require __DIR__ . "/../partials/data.php";
 header("Content-Type:application/json");
 
+
 if(!empty($_GET['genre'])){
+
+    //salvo info che arriverà in GET in una var
     $genre = $_GET['genre'];
+    //creo un ARR per album filtrati
     $dbFiltered = [];
 
+    //ciclo su file JSON
     foreach($database as $album){
+
+        //se il valore del GET è uguale al valore genre del file con i dati
         if($genre == $album['genre']){
+
+            //allora "filtro" file con i dati e il risultato salvo in Arr che ho creato prima ($dbFiltered)
             $dbFiltered[] = $album; 
         }
     }
+    //se c'e' qualcosa che arriva in GET allora "trasformo" il file filtrato con i dati in file JSON
     echo json_encode($dbFiltered);
 }else{
+    //altrimenti "trasformo" il file originale con i dati in file JSON
     echo json_encode($database);
 }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 const app = new Vue({
   el: "#app",
   data: {
+    //creo due arr dove andranno i dati che arriveranno da API
     albums: [],
     genres: [],
     select: "",
 
   },
+
+  //Chiamo il file JSON originale con tutti album
   created() {
     axios.get('http://localhost/api/index.php')
       .then((response) => {
         this.albums = response.data;
       })
 
+
+      //chiamo api per prendere info sui generi e li salvo in un array 
       axios.get('http://localhost/api/index.php?genres=genres')
       .then((response) => {
         this.genres = response.data;
@@ -19,10 +24,15 @@ const app = new Vue({
 
   },
   methods: {
+     //Al cambio della select (in file html )chiamo il file JSON filtrato di album
     onChange(event) {
+
+      //aseegno alla variable "select" il valore della select (quello che sceglie utente)
       this.select = event.target.value;
       axios.get('http://localhost/api/index.php', {
         params: {
+
+          //specifico il parametro da considerare per la stampa degli album
           genre: this.select,
         },
       })
